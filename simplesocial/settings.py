c291be4db52148ceee130c2511b97b0056bb95a3
RedoneRony <chokers547@gmail.com>
2022-01-22 09:42:18 +0600

    update i

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -127,3 +127,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
